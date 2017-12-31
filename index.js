ed7400fe6476e48de4d203b844d076b81067c954
Daniel Roberts <BeardOfDan@gmail.com>
2017-12-30 18:18:50 -0800

    (feat) Host the game on a node server

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public/'));
+
+app.listen(PORT, () => {
+  console.log('Now listening on port ' + PORT);
+});
